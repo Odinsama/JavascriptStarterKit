3b61e899b22e6f024d59fc870f5ec1d2108b55e9
Odin Thorsen <odin.thorsen@genus.no>
2017-08-25 10:52:22 +0200

    finished the course without using a cloud server or error log js in deployment

---
+++ webpack.config.prod.js
@@ -0,0 +1,46 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+export default {
+  debug: true,
+  devtool: 'source-map',
+  noInfo: false,
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].[chunkhash].js'
+  },
+  alias: [],
+  plugins: [
+
+    new ExtractTextPlugin('[name].[contenthash].css'),
+
+    new WebpackMd5Hash(),
+    // Use CommonsChunkPlugin to create a separate bundle
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+
+    // Create HTML file that includes reference to bundled JS.
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: true
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
+    ]
+  }
+}
